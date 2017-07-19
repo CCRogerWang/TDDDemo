f44f142470de63bdf26969d99ae94a6f1c0802a8
roger.wang[王濬淇] <roger.wang@104.com.tw>
2017-07-19 14:41:49 +0800

    init demo TDD

---
+++ GetGeoLocation/InputViewController.swift
@@ -0,0 +1,14 @@
+//
+//  InputViewController.swift
+//  GetGeoLocation
+//
+//  Created by roger on 2017/7/19.
+//  Copyright © 2017年 roger. All rights reserved.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+
+
+}
---
+++ GetGeoLocationTests/InputViewControllerTests.swift
@@ -0,0 +1,22 @@
+//
+//  InputViewControllerTests.swift
+//  GetGeoLocation
+//
+//  Created by roger on 2017/7/19.
+//  Copyright © 2017年 roger. All rights reserved.
+//
+
+import XCTest
+
+class InputViewControllerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    
+}
